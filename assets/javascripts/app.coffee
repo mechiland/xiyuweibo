d9f1383efbf42e37cad8d03c7c2b86679726976e
Michael Chen <mechiland@gmail.com>
2012-03-01 01:53:20 -0600

    now can slide

--- assets/javascripts/app.coffee
+++ assets/javascripts/app.coffee
@@ -2,17 +2,25 @@ delay = (ms, func) -> setTimeout func, ms
 repeat = (ms, func) -> setInterval func, ms
 
 $ ->
-  ($ ".container").attr("style", "height: " + (window.innerHeight - 36) + "px")
+  _last = null
+  $(".container").attr("style", "height: " + (window.innerHeight - 36) + "px")
+  $(".bo_container").click ->
+    if _last != null
+      _last.removeClass("selected")
+      $('.sub_container').animate({"left": "0px"}, "fast").hide()
+      
+    _last = $(this)
+    $(this).addClass("selected")
+    $('.sub_container').show().animate({"left": $(this).width() + "px"}, "fast")
   $(window).resize ->
-    console.log(window.innerHeight)
     $(".container").attr("style", "height: " + (window.innerHeight - 36) + "px")
-    console.log("Its ready");
-    $("#btn").click ->
-      macgap.window.open({url: "./auth_sina.html", width: 640, height: 480})
-      l = macgap.window.open({url: "public/auth_sina.html", width: 640, height: 480})
-      repeat 1000, -> console.log(l.url())
-      macgap.window.open({url:"http://scottjehl.github.com/Respond/test/test.html", width: 500, height: 400})
 
-      delay 1000, -> macgap.window.resize({width: 500, height: 300})
+  $("#btn").click ->
+    macgap.window.open({url: "./auth_sina.html", width: 640, height: 480})
+    l = macgap.window.open({url: "public/auth_sina.html", width: 640, height: 480})
+    repeat 1000, -> console.log(l.url())
+    macgap.window.open({url:"http://scottjehl.github.com/Respond/test/test.html", width: 500, height: 400})
 
-      console.log("========" + macgap.window.url())
+    delay 1000, -> macgap.window.resize({width: 500, height: 300})
+
+    console.log("========" + macgap.window.url())
--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -10,39 +10,52 @@
   };
 
   $(function() {
-    ($(".container")).attr("style", "height: " + (window.innerHeight - 36) + "px");
-    return $(window).resize(function() {
-      console.log(window.innerHeight);
-      $(".container").attr("style", "height: " + (window.innerHeight - 36) + "px");
-      console.log("Its ready");
-      return $("#btn").click(function() {
-        var l;
-        macgap.window.open({
-          url: "./auth_sina.html",
-          width: 640,
-          height: 480
-        });
-        l = macgap.window.open({
-          url: "public/auth_sina.html",
-          width: 640,
-          height: 480
-        });
-        repeat(1000, function() {
-          return console.log(l.url());
-        });
-        macgap.window.open({
-          url: "http://scottjehl.github.com/Respond/test/test.html",
-          width: 500,
-          height: 400
-        });
-        delay(1000, function() {
-          return macgap.window.resize({
-            width: 500,
-            height: 300
-          });
-        });
-        return console.log("========" + macgap.window.url());
+    var _last;
+    _last = null;
+    $(".container").attr("style", "height: " + (window.innerHeight - 36) + "px");
+    $(".bo_container").click(function() {
+      if (_last !== null) {
+        _last.removeClass("selected");
+        $('.sub_container').animate({
+          "left": "0px"
+        }, "fast").hide();
+      }
+      _last = $(this);
+      $(this).addClass("selected");
+      return $('.sub_container').show().animate({
+        "left": $(this).width() + "px"
+      }, "fast");
+    });
+    $(window).resize(function() {
+      return $(".container").attr("style", "height: " + (window.innerHeight - 36) + "px");
+    });
+    return $("#btn").click(function() {
+      var l;
+      macgap.window.open({
+        url: "./auth_sina.html",
+        width: 640,
+        height: 480
       });
+      l = macgap.window.open({
+        url: "public/auth_sina.html",
+        width: 640,
+        height: 480
+      });
+      repeat(1000, function() {
+        return console.log(l.url());
+      });
+      macgap.window.open({
+        url: "http://scottjehl.github.com/Respond/test/test.html",
+        width: 500,
+        height: 400
+      });
+      delay(1000, function() {
+        return macgap.window.resize({
+          width: 500,
+          height: 300
+        });
+      });
+      return console.log("========" + macgap.window.url());
     });
   });
 
